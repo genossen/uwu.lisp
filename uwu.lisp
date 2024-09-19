ec3724fa2c111f2b2a5a1a03e3b2b21d7c1b1667
Blair Vidak <vidak@riseup.net>
2024-09-19 17:27:58 +0800

    started work on the shop function(s)

--- uwu.lisp
+++ uwu.lisp
@@ -28,4 +28,47 @@
       (subseq "          " *movement*)
       (nth rng-move (cdar pet-gfx))))))
 
+(defun pet-shop ()
+  (loop do
+    (princ "This is the shop.") (terpri)
+    (sleep 1)
+    (princ "Options: > FOOD") (terpri)
+    (princ "         > MEDICINE") (terpri)
+    (princ "         > GROWTH") (terpri)
+    (princ "         > CHANGE") (terpri)
+    (princ "         > EXIT") (terpri)
+    (sleep 1)
+    (princ "> ")
 
+    (when
+	(eq (read) 'EXIT) (return))
+
+    (when
+	(eq (read) 'FOOD) (pet-shop-menu-food))))
+
+(defvar *spicy-food* "SPICY food.")
+(defvar *sour-food* "SOUR food.")
+(defvar *salty-food* "SALTY food.")
+(defvar *sweet-food* "SWEET food.")
+(defvar *savoury-food* "SAVOURY food.")
+
+(defun pet-shop-menu-food ()
+  (loop do
+    
+    (princ "This is the food ~") (terpri)
+    (sleep 1)
+    (princ "Options: > ") (princ *spicy-food*) (terpri)
+    (princ "         > ") (princ *sour-food*) (terpri)
+    (princ "         > ") (princ *salty-food*) (terpri)
+    (princ "         > ") (princ *sweet-food*) (terpri)
+    (princ "         > ") (princ *savoury-food*) (terpri)
+    (sleep 1)
+    (princ "> ")
+
+    (let ((input (read)))
+      (cond ((eq input 'SPICY) (print "spicy"))
+	    ((eq input 'SOUR) (print "sour"))
+	    ((eq input 'SALTY) (print "salty"))
+	    ((eq input 'SWEET) (print "sweet"))
+            ((eq input 'SAVOURY) (print "savoury"))
+	    ((eq input 'EXIT) (return))))))
